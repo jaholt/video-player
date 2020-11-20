d91304e5aa1e3a4946aaa4bd318ca0f8da5cd101
Joe Holt <holtj@JosephXPSTower.private.halfways.net>
2020-11-20 11:22:50 +0000

    added the ability to control the volume via clicking the volume bar and added dyanmic changing of the volume icon to represent the changed volume

--- script.js
+++ script.js
@@ -56,6 +56,28 @@ function setProgress (e) {
 
 // Volume Controls --------------------------- //
 
+// Volume bar
+function changeVolume (e) {
+    // controlling the volume via clicking the bar
+    let volume = e.offsetX / volumeRange.offsetWidth
+    if (volume < 0.1) {
+        volume = 0
+    }
+    if (volume > 0.9) {
+        volume = 1
+    }
+    volumeBar.style.width = `${volume * 100}%`
+    video.volume = volume
+    // change the volume icon depending on volume level
+    volumeIcon.className = ''
+    if (volume > 0.7) {
+        volumeIcon.classList.add('fas', 'fa-volume-up')
+    } else if (volume < 0.7 && volume > 0) {
+        volumeIcon.classList.add('fas', 'fa-volume-down')
+    } else if (volume === 0) {
+        volumeIcon.classList.add('fas', 'fa-volume-off')
+    }
+}
 
 
 // Change Playback Speed -------------------- //
@@ -71,3 +93,4 @@ video.addEventListener('click', togglePlay)
 video.addEventListener('timeupdate', updateProgress)
 video.addEventListener('canplay', updateProgress)
 progressRange.addEventListener('click', setProgress)
+volumeRange.addEventListener('click', changeVolume)
